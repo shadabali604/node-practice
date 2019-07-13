bf093796560ab3372e6a53f9d79018bb282d4c35
shadabali604 <shadabali604@gmail.com>
2019-07-13 08:35:33 +0530

    Objec Decontructor use in project

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -4,16 +4,16 @@ const forecast = (latitude, longitude, callback) => {
     const url = 'https://api.darksky.net/forecast/2d6d10942d735841437adde9f9df433c/' + latitude + ',' + longitude
 
     request({
-        url: url,
+        url,
         json: true
-    }, (error, response) => {
+}, (error, {body}) => {
         if (error) {
             callback('unable to connect to location', undefined)
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('unable to find location, try anther search', undefined)
         } else {
-            const data = response.body.currently
-            callback(undefined, response.body.daily.data[0].summary + "teampture is " + +data.temperature + " " + " there is only " + " " + data.precipProbability + " Chance For Rain")
+            const data = body.currently
+            callback(undefined, body.daily.data[0].summary + "teampture is " + +data.temperature + " " + " there is only " + " " + data.precipProbability + " Chance For Rain")
         }
     })
 }
--- weather-app/utils/geocode.js
+++ weather-app/utils/geocode.js
@@ -4,18 +4,18 @@ const request = require('request')
 const geocode = (address, callback) => {
     const url = "https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2hhZGFiMjYiLCJhIjoiY2p4encyMTVnMDZ4MzNocGt6aHA0cno4aSJ9.DwCYYdSS_1Mgk3ZuEEPrEg";
     
-    request({url: url, json:true}, (error, response) => {
+    request({url, json:true}, (error, {body}) => {
         if(error){
             callback('unable to connect to loction', undefined)
         }
-        else if(response.body.features.length === 0){
+        else if(body.features.length === 0){
             callback('Unable to find location, Try another search', undefined)
         }
         else{
             callback(undefined, {
-                latitude: response.body.features[0].center[1],
-                longitude:response.body.features[0].center[0],
-                location: response.body.features[0].place_name
+                latitude: body.features[0].center[1],
+                longitude:body.features[0].center[0],
+                location: body.features[0].place_name
             })
         }
          
