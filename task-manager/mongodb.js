7b4026c2d26fe89c9d5db515dc24f7d15a0a79f3
shadabali604 <shadabali604@gmail.com>
2019-07-17 17:10:13 +0530

    add some mongo

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,11 +1,42 @@
+// CRUD create read update delete
+
 const mongodb = require('mongodb')
 const MongoClient = mongodb.MongoClient
+
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true}, (error, client) => {
-if(error){
-    console.log('unable to connect to database')
-}
-console.log("connect Correctly")
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log(error)
+    }
+    console.log('Conneced correctly')
 })
+
+     
+    // db.collection('users').insertOne({
+    //     name: 'Andrew',
+    //     age: 27
+    // }, (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert user')
+    //     }
+
+    //     console.log(result.ops)
+    // })
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Jen',
+    //         age: 28
+    //     }, {
+    //         name: 'Gunther',
+    //         age: 27
+    //     }
+    // ], (error, result) => {
+    //     if (error) {
+    //         return console.log('Unable to insert documents!')
+    //     }
+
+    //     console.log(result.ops)
+    // })
