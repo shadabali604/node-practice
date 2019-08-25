2148ea5424299019ab85d07d9b04d509adf47919
shadabali604 <shadabali604@gmail.com>
2019-08-25 13:20:18 +0530

    add put method

--- basic/restapi/index.js
+++ basic/restapi/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const Joi = require('@hapi/joi');
+
 
 
 const app = express();
-app.use()
+app.use(express.json());
 const persons = [
     {name:'Shadab1', id: 1}, 
     {name:'Shadab2', id: 2},
@@ -23,14 +25,37 @@ app.get('/api/person/:id', (req, res) => {
  res.send(person);
 });
 app.post('/api/person', (req, res) => {
-    const person = {
+    const schema = Joi.object().keys({
+        name: Joi.string().alphanum().min(3).max(30).required() });
+    
+        const result = Joi.validate(req.body, schema);
+    
+    if(result.error) {
+        res.status.send(400).send('Fond  ')
+    }
+        const person = {
         id: persons.id + 1,
         name: req.body.name
     };
     person.push(person);
-    res.send(person);
+    res.send(person); 
 });
 
+
+app.put('/api/person/:id',(req, res) => {
+
+    const schema = Joi.object().keys({
+        name: Joi.string().alphanum().min(3).max(30).required() });
+    
+        const result = Joi.validate(req.body, schema);
+
+   const person = persons.find(c => c.id === parseInt(req.param.id));
+    if(!person){
+        req.status(404).send('Recod not found')
+    }
+     person.name = req.body.name;
+     req.send(person);
+});
 
 app.listen(3000, () => {
     console.log('Working Good')
