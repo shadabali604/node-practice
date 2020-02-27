bed137b142cd646f2731b792becf9b90155b890c
Shadab Ali <shadabali604@gmail.com>
2020-02-27 19:32:36 +0530

    schema

--- REST2/models/user.js
+++ REST2/models/user.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
     name:{
         type:String,
-        minlength: 8
+        minlength: 6
     },
-    last:{
+    email:{
      type:String,
+     minlength:12
 
-
+    },
+    password:{
+        type:String,
+        minlength:8
     }
 });
 
